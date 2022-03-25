a47fb245cf7826df9b4fcd1c0a5a42a5726c924c
naumanshabbir2811 <naumanshabbir19@gmail.com>
2022-03-25 22:31:20 +0500

    chnages

--- src/Service.js
+++ src/Service.js
@@ -101,7 +101,7 @@ function Service() {
                 </svg>
               </div>
               <div className="service__meta">
-                <h1 className="service__text">Marketing & Reporting</h1>
+                <h1 className="service__text">Application Deployment</h1>
                 <p className="p service__text p__color">
                   I throw myself down among the
                 </p>
