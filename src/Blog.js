095987ccabf4fe77f32754a693ac33b50e25c930
naumanshabbir2811 <naumanshabbir19@gmail.com>
2022-03-26 17:34:04 +0500

    changes

--- src/Blog.js
+++ src/Blog.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Blog.css";
-import Blog1 from './img/blog-01.jpg';
+import Blog1 from './img/blog-1.jpg';
 import Blog2 from './img/blog-02.jpg';
 import Blog3 from './img/blog-03.jpg';
 
